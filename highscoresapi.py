f63d2a075e37a64039dbc5a83c818e7e07b209b8
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-05-30 11:31:14 +0200

    Added a blueprint for the highscoreapi

---
+++ highscoresapi.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+highscores_api = Blueprint('highsccores_api', __name__)
+
+
+@highscores_api.route('/game1')
+def game1():
+    return 'game1'
--- polarbirdshub.py
+++ polarbirdshub.py
@@ -1,7 +1,10 @@
 from flask import g, render_template, flash, Flask, session, redirect, url_for, \
     request
+from highscoresapi import highscores_api
 
 app = Flask(__name__)
+
+app.register_blueprint(highscores_api, url_prefix='/highscores')
 
 
 @app.route('/')
