fd8945abd7c6a07da1f0f2aa0f15f5da2d891784
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-06-01 12:32:03 +0200

    Added some game dummy-data

--- dummydata.sql
+++ dummydata.sql
@@ -1,1 +1,5 @@
 -- empty
+INSERT INTO lb_game('name') VALUES ('NT');
+INSERT INTO lb_game('name') VALUES ('NT2');
+INSERT INTO lb_game('name') VALUES ('NT3');
+INSERT INTO lb_game('name') VALUES ('NT4');
