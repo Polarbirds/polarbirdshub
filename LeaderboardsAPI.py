e1f68918362d33785493e3d7b0a03f84882fa840
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-06-01 12:34:42 +0200

    Added root-route for leaderboards which displays lb_game-table contents

--- LeaderboardsAPI.py
+++ LeaderboardsAPI.py
@@ -1,10 +1,18 @@
 from flask import Blueprint
+from flask.templating import render_template
 
 leaderboards_api = Blueprint('leaderboards_api', __name__)
 
 
-@leaderboards_api.route('/game1')
-def game1():
+@leaderboards_api.route('/')
+def leaderboards():
+    import polarbirdshub as hub
+    games = hub.query_db('SELECT * from lb_game')
+    return render_template('leaderboards.html', entries=games)
+
+
+@leaderboards_api.route('/<gamename>')
+def game1(gamename):
     return 'game1'
 
 
