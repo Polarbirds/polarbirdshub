1ed945fae7e78395e53e7ec3a3e36b50cc6022df
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-08-09 20:26:20 +0200

    google claim ownership

--- polarbirdshub.py
+++ polarbirdshub.py
@@ -126,5 +126,10 @@ def github_hook():
     return 'ok'
 
 
+@app.route('google3b4c7eb337ddee7b.html')
+def googlestuff():
+    return render_template('google3b4c7eb337ddee7b.html')
+
+
 if __name__ == '__main__':
     app.run()
